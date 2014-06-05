4370b1585ae96218a5f60c1a16a391e970038078
Szymon Fiedler <fidelski@gmail.com>
2014-06-05 19:33:34 +0200

    Solved custom delimeters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,13 +3,42 @@ class StringCalculator
     extract_numbers(expression).reduce(&:+).to_i
   end
 
+  class ExpressionParser
+    def initialize(expression)
+      @expression = expression
+    end
+
+    def numbers_string
+      parse_expression[1]
+    end
+
+    def delimeter
+      parse_expression[0] || default_delimeters
+    end
+
+    def to_s
+      numbers_string
+    end
+
+    private
+
+    attr_reader :expression
+
+    def default_delimeters
+      %r{,|\n}
+    end
+
+    def parse_expression
+      _, custom_delimeter, numbers_string = /(\/\/(.{1})?)[\n]?(.*){1}/m.match(expression).captures
+      return [custom_delimeter, numbers_string]
+    end
+  end
+
   private
 
   def extract_numbers(expression)
-    expression.split(allowed_delimiters).map(&:to_i)
+    parser = ExpressionParser.new(expression)
+    parser.to_s.split(parser.delimeter).map(&:to_i)
   end
 
-  def allowed_delimiters
-    %r{,|\n}
-  end
 end
